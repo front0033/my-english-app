a12f5c1e9d577e908ae59dd1af99984ab7cf65f2
Igor.Bezdenezhnykh <Igor.Bezdenezhnykh@x5.ru>
2021-05-19 23:13:10 +0300

    code

--- src/pages/MainPage/dictionaries/code.ts
+++ src/pages/MainPage/dictionaries/code.ts
@@ -56,6 +56,247 @@ export default {
       translate: 'однопоточный',
     },
     {
+      word: 'expression',
+      translate: 'выражение',
+    },
+    {
+      word: 'declaration',
+      translate: 'декларация',
+    },
+    {
+      word: 'implementation',
+      translate: 'выполнение',
+    },
+    {
+      word: 'immediately invoked function expression (IIFE)',
+      translate: 'выражение немедленно вызываемой функции',
+    },
+    {
+      word: 'execute',
+      translate: 'выполнять',
+    },
+    {
+      word: 'self-calling function',
+      translate: 'самовызывающаяся функция',
+    },
+    {
+      word: 'parentheses',
+      translate: 'круглые скобки',
+    },
+    {
+      word: 'condition',
+      translate: 'условие',
+    },
+    {
+      word: 'exception',
+      translate: 'исключение',
+    },
+    {
+      word: 'reference',
+      translate: 'ссылка',
+    },
+    {
+      word: 'instance',
+      translate: 'экземпляр объекта',
+    },
+    {
+      word: 'performance',
+      translate: 'представление, производительность',
+    },
+    {
+      word: 'pointer',
+      translate: 'указатель',
+    },
+    {
+      word: 'definition',
+      translate: 'определение',
+    },
+    {
+      word: 'environment',
+      translate: 'окружение',
+    },
+
+    {
+      word: 'arrow function',
+      translate: 'стрелочная функция',
+    },
+    {
+      word: 'concatenating',
+      translate: 'сцепление, связывание',
+    },
+    {
+      word: 'check out',
+      translate: 'отстроиться',
+    },
+    {
+      word: 'essential',
+      translate: 'основной, необходимый, ценный',
+    },
+    {
+      word: 'resolution',
+      translate: 'решение',
+    },
+    {
+      word: 'runtime',
+      translate: 'время выполнения',
+    },
+    {
+      word: 'empty',
+      translate: 'empty',
+    },
+    {
+      word: 'empty',
+      translate: 'empty',
+    },
+    {
+      word: 'empty',
+      translate: 'empty',
+    },
+    {
+      word: 'empty',
+      translate: 'empty',
+    },
+    {
+      word: 'empty',
+      translate: 'empty',
+    },
+    {
+      word: 'empty',
+      translate: 'empty',
+    },
+    {
+      word: 'empty',
+      translate: 'empty',
+    },
+    {
+      word: 'empty',
+      translate: 'empty',
+    },
+    {
+      word: 'empty',
+      translate: 'empty',
+    },
+    {
+      word: 'empty',
+      translate: 'empty',
+    },
+    {
+      word: 'empty',
+      translate: 'empty',
+    },
+    {
+      word: 'empty',
+      translate: 'empty',
+    },
+    {
+      word: 'empty',
+      translate: 'empty',
+    },
+    {
+      word: 'empty',
+      translate: 'empty',
+    },
+    {
+      word: 'empty',
+      translate: 'empty',
+    },
+    {
+      word: 'empty',
+      translate: 'empty',
+    },
+    {
+      word: 'empty',
+      translate: 'empty',
+    },
+    {
+      word: 'empty',
+      translate: 'empty',
+    },
+    {
+      word: 'empty',
+      translate: 'empty',
+    },
+    {
+      word: 'empty',
+      translate: 'empty',
+    },
+    {
+      word: 'empty',
+      translate: 'empty',
+    },
+    {
+      word: 'empty',
+      translate: 'empty',
+    },
+    {
+      word: 'empty',
+      translate: 'empty',
+    },
+    {
+      word: 'empty',
+      translate: 'empty',
+    },
+    {
+      word: 'empty',
+      translate: 'empty',
+    },
+    {
+      word: 'empty',
+      translate: 'empty',
+    },
+    {
+      word: 'empty',
+      translate: 'empty',
+    },
+    {
+      word: 'empty',
+      translate: 'empty',
+    },
+    {
+      word: 'empty',
+      translate: 'empty',
+    },
+    {
+      word: 'empty',
+      translate: 'empty',
+    },
+    {
+      word: 'empty',
+      translate: 'empty',
+    },
+    {
+      word: 'empty',
+      translate: 'empty',
+    },
+    {
+      word: 'empty',
+      translate: 'empty',
+    },
+    {
+      word: 'empty',
+      translate: 'empty',
+    },
+    {
+      word: 'empty',
+      translate: 'empty',
+    },
+    {
+      word: 'empty',
+      translate: 'empty',
+    },
+    {
+      word: 'empty',
+      translate: 'empty',
+    },
+    {
+      word: 'empty',
+      translate: 'empty',
+    },
+    {
+      word: 'empty',
+      translate: 'empty',
+    },
+    {
       word: 'empty',
       translate: 'empty',
     },
