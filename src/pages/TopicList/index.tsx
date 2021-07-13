a68fb5ed6f75e404e238db2a1ba0c95d57b78e7b
Igor.Bezdenezhnykh <Igor.Bezdenezhnykh@x5.ru>
2021-07-14 00:14:41 +0300

    topics page

--- src/pages/TopicList/index.tsx
+++ src/pages/TopicList/index.tsx
@@ -1,12 +1,54 @@
 import * as React from 'react';
 
-import {Grid} from '@material-ui/core';
+import {
+  Grid,
+  Typography,
+  LinearProgress,
+  List,
+  ListItem,
+  ListItemText,
+  ListItemSecondaryAction,
+  IconButton,
+} from '@material-ui/core';
+import EditIcon from '@material-ui/icons/Edit';
+import {Alert} from '@material-ui/lab';
+import {useGetTopics} from 'redux/stores/topics/topicSlice';
 import useStyles from './styles';
 
 const TopicList: React.FC = () => {
   const classes = useStyles();
+  const {data, isSuccess: isTopicSuccess, isLoading: isTopicLoading, isError: isTopicsError} = useGetTopics({});
 
-  return <Grid className={classes.container}>TopicList</Grid>;
+  const topics = data?.topics ?? [];
+
+  return (
+    <Grid className={classes.container}>
+      {isTopicsError && <Alert severity="error">topics: server error</Alert>}
+      {isTopicLoading && (
+        <Grid container direction="column" alignItems="center">
+          <Typography className={classes.progressCaption} variant="caption">
+            Loading topics...
+          </Typography>
+          <LinearProgress className={classes.progress} />
+        </Grid>
+      )}
+      {isTopicSuccess && !topics.length && <Alert severity="info">Topics are missing.</Alert>}
+      {isTopicSuccess && !!topics.length && (
+        <List className={classes.width100}>
+          {topics.map(topic => (
+            <ListItem divider key={topic.id}>
+              <ListItemText id={topic.id} primary={topic.name} />
+              <ListItemSecondaryAction>
+                <IconButton>
+                  <EditIcon />
+                </IconButton>
+              </ListItemSecondaryAction>
+            </ListItem>
+          ))}
+        </List>
+      )}
+    </Grid>
+  );
 };
 
 export default React.memo(TopicList);
