a75c29fe8cded1dafe5f2fc7817d2d8f586a3414
Igor.Bezdenezhnykh <Igor.Bezdenezhnykh@x5.ru>
2021-07-17 16:54:19 +0300

    delete dialog

--- src/components/DeleteButtonWithConfirmDialog/index.tsx
+++ src/components/DeleteButtonWithConfirmDialog/index.tsx
@@ -1,28 +1,28 @@
 import * as React from 'react';
 
-import { IconButton, Popover, Grid, Typography, makeStyles, createStyles, Button } from '@material-ui/core';
+import {
+  IconButton,
+  Typography,
+  makeStyles,
+  createStyles,
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+} from '@material-ui/core';
 import DeleteForeverIcon from '@material-ui/icons/DeleteForever';
-import CheckCircleOutlineIcon from '@material-ui/icons/CheckCircleOutline';
-import CancelIcon from '@material-ui/icons/Cancel';
 
 interface IDeleteButtonWithConfirmDialogProps {
   id: string;
   text: string;
-  buttonText?: string;
   action: () => void;
 }
 
 const useStyles = makeStyles((theme) =>
   createStyles({
-    popover: {
-      position: 'relative',
-    },
-    content: {
-      margin: theme.spacing(1.5, 3),
-      width: 100,
-    },
-    text: {
-      paddingBottom: theme.spacing(1),
+    container: {
+      width: 200,
     },
     icon: {
       color: theme.palette.secondary.main,
@@ -30,20 +30,13 @@ const useStyles = makeStyles((theme) =>
   })
 );
 
-const PureDeleteButtonWithConfirmDialog: React.FC<IDeleteButtonWithConfirmDialogProps> = ({
-  id,
-  text,
-  buttonText,
-  action,
-}) => {
+const PureDeleteButtonWithConfirmDialog: React.FC<IDeleteButtonWithConfirmDialogProps> = ({ id, text, action }) => {
   const classes = useStyles({});
 
   const [open, setOpen] = React.useState<boolean>(false);
-  const [anchor, setAnchor] = React.useState<Element | null>(null);
 
   const handleCancelClick = () => setOpen(false);
-  const handleConfirmClick = (event: React.MouseEvent) => {
-    setAnchor(event.currentTarget);
+  const handleConfirmClick = () => {
     setOpen(true);
   };
 
@@ -54,32 +47,32 @@ const PureDeleteButtonWithConfirmDialog: React.FC<IDeleteButtonWithConfirmDialog
 
   return (
     <>
-      {buttonText ? (
-        <Button size="small" id={`delete-button-delete-${id}`} color="secondary" onClick={handleConfirmClick}>
-          {buttonText}
-        </Button>
-      ) : (
-        <IconButton id={`delete-icon-button-delete-${id}`} color="secondary" onClick={handleConfirmClick}>
-          <DeleteForeverIcon className={classes.icon} />
-        </IconButton>
+      <IconButton id={`delete-icon-button-delete-${id}`} color="secondary" onClick={handleConfirmClick}>
+        <DeleteForeverIcon className={classes.icon} />
+      </IconButton>
+      {!!open && (
+        <Dialog
+          open={open}
+          onClose={handleCancelClick}
+          aria-labelledby="draggable-dialog-title"
+          fullWidth
+          maxWidth="xs"
+        >
+          <DialogContent>
+            <DialogContentText>
+              <Typography>{text}</Typography>
+            </DialogContentText>
+          </DialogContent>
+          <DialogActions>
+            <Button autoFocus onClick={handleCancelClick} color="primary">
+              Cancel
+            </Button>
+            <Button onClick={handleActionClick} color="primary">
+              Delete
+            </Button>
+          </DialogActions>
+        </Dialog>
       )}
-      <Popover open={open} anchorEl={anchor}>
-        <Grid id={`delete-dialog-${id}`} container className={classes.content}>
-          <Grid item xs={12} className={classes.text}>
-            <Typography>{text}</Typography>
-          </Grid>
-          <Grid item xs={6}>
-            <IconButton id={`delete-dialog-button-cancel-${id}`} onClick={handleCancelClick}>
-              <CancelIcon color="secondary" />
-            </IconButton>
-          </Grid>
-          <Grid item xs={6} container justify="flex-end">
-            <IconButton id={`delete-dialog-button-ok-${id}`} color="primary" onClick={handleActionClick}>
-              <CheckCircleOutlineIcon />
-            </IconButton>
-          </Grid>
-        </Grid>
-      </Popover>
     </>
   );
 };
--- src/pages/TopicList/index.tsx
+++ src/pages/TopicList/index.tsx
@@ -61,7 +61,7 @@ const TopicList: React.FC = () => {
                 </IconButton>
                 <DeleteButtonWithConfirmDialog
                   id={`topic-delete-button-${topic.id}`}
-                  text="Are you sure?"
+                  text={`Are you sure you want to delete «${topic.name}?»`}
                   action={handleDeleteById(topic.id)}
                 />
               </ListItemSecondaryAction>
